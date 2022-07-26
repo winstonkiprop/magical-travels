5883ae4a9997e8bca7529371f330e547491b3cec
winston kiprop <winstonekip50@gmail.com>
2022-07-26 18:20:01 +0300

    created nav-bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+
+function NavBar(){
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink>
+                    </li>
+                    <li>
+                        <NavLink></NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
