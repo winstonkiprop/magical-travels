5aab0e287fb881f40b32c096a31203dd007d9f55
winston kiprop <winstonekip50@gmail.com>
2022-07-30 09:36:21 +0300

    additing more travel options

--- src/components/NewTravel.js
+++ src/components/NewTravel.js
@@ -1,10 +1,36 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 function NewReview(){
+    const [user, setUser] = useState("")
+            const [content, setContent] = useState("")
+
+            const  handleFormSubmit = (e) => {
+                e.preventDefault();
+
+
+                axios.post(' http://localhost:3000/blog',{ 
+                user:user,
+                content:content})
+                .then((response) => {
+                    console.log(response)
+                }, (error) => {
+                    console.log(error);
+                })
+
+            };
     return(
         <div>
-            
+            <form className="new-review-form" onSubmit={handleFormSubmit}>
+                <input placeholder="user" type="text" 
+                value={user} onChange={(e) => setUser(e.target.value)}/>
+                <textarea placeholder="add a new review" rows={10} type="text" value={content}
+                onChange={(e) => setContent(e.target.value)}/>
+                <input type="submit" value="add review"/>
+
+            </form>
         </div>
     )
 }
+
+export default NewReview;
