0f17dd49a4c1ab327a4fa68649ae8dc486e6171f
winston kiprop <winstonekip50@gmail.com>
2022-07-30 09:34:30 +0300

    styling home file

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,19 @@ import React from "react";
 
 function Home(){
     const imgSrc = "https://www.awf.org/sites/default/files/2020-01/tourism_Paul_Lampert%20%281%29.jpg";
+    //const imgSc= "https://www.serengeti.com/assets/img/lion-serengeti-africa.jpg";
     return(
         <div className="home">
+        
+            <h2>Home to beautiful scenery in and around Africa </h2>
             <p>Get accurate and reliable tour travel details and costs to the perfect destination.</p>
             <img src={imgSrc} />
+             
+             
+            
         </div>
+
+        
     )
 }
 
