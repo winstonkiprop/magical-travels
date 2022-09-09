7324213ed780ae40bbab8cb3400f7d8dd37ef1ab
winston kiprop <winstonekip50@gmail.com>
2022-09-09 08:34:04 +0300

    added a crud operation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,20 @@
-import React from "react";
-
+import React, {useState} from "react";
+import NewDestination from "./NewDestination";
+import DeleteReview from "./DeleteReview";
 function Home(){
-    const imgSrc = "https://www.awf.org/sites/default/files/2020-01/tourism_Paul_Lampert%20%281%29.jpg";
-    //const imgSc= "https://www.serengeti.com/assets/img/lion-serengeti-africa.jpg";
+    let [show, setForm] = useState(false);
+    function handleClick(){
+        setForm(true);
+    }
     return(
         <div className="home">
-        
             <h2>Home to beautiful scenery in and around Africa </h2>
-            <p>Get accurate and reliable tour travel details and costs to the perfect destination.</p>
-            <img src={imgSrc} />  
-            
+            <p>Get accurate and reliable tour travel details and costs to the perfect destination.<br/>
+            Our purpose is to give you exquisite tour experiences that will create pleasurable memories for you and your loved ones.
+            </p>
+            <button className="add-new-dest" onClick={handleClick}>Add a Destination</button>
+            {show ? <NewDestination/> : null}
         </div>
-
-        
     )
 }
 
